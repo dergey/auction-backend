87b817f6b0e5f104c66a1d0f15d3fc590e357338
Sergey Zhuravlev <zhuravlev@mail.scand>
2018-10-12 15:58:44 +0300

    Configured session work

--- src/main/java/com/sergey/zhuravlev/auctionserver/config/WebSecurityConfigure.java
+++ src/main/java/com/sergey/zhuravlev/auctionserver/config/WebSecurityConfigure.java
@@ -1,8 +1,6 @@
 package com.sergey.zhuravlev.auctionserver.config;
 
 import com.sergey.zhuravlev.auctionserver.repository.UserRepository;
-import com.sergey.zhuravlev.auctionserver.security.MySavedRequestAwareAuthenticationSuccessHandler;
-import com.sergey.zhuravlev.auctionserver.security.RestAuthenticationEntryPoint;
 import com.sergey.zhuravlev.auctionserver.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -13,49 +11,52 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
 
     private final UserRepository userRepository;
-    private final RestAuthenticationEntryPoint restAuthenticationEntryPoint;
-    private final MySavedRequestAwareAuthenticationSuccessHandler authenticationSuccessHandler;
 
     @Autowired
-    public WebSecurityConfigure(UserRepository userRepository,
-                                RestAuthenticationEntryPoint restAuthenticationEntryPoint,
-                                MySavedRequestAwareAuthenticationSuccessHandler authenticationSuccessHandler) {
+    public WebSecurityConfigure(UserRepository userRepository) {
         super();
         this.userRepository = userRepository;
-        this.restAuthenticationEntryPoint = restAuthenticationEntryPoint;
-        this.authenticationSuccessHandler = authenticationSuccessHandler;
     }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.
-                userDetailsService(userDetailsService()).
-                passwordEncoder(passwordEncoder());
+        auth
+            .userDetailsService(userDetailsService())
+            .passwordEncoder(passwordEncoder());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+
         http
-                .csrf().disable()
-                .exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint)
-                .and()
-                    .formLogin()
-                        .successHandler(authenticationSuccessHandler)
-                        .failureHandler(new SimpleUrlAuthenticationFailureHandler())
-                .and()
-                    .httpBasic()
-                .and()
-                    .logout();
+            .csrf().disable()
+            .formLogin()
+                .loginProcessingUrl("/login")
+            .and()
+            .logout()
+                .deleteCookies("JSESSIONID")
+            .and()
+            .rememberMe()
+                .key("uniqueAndSecret").tokenValiditySeconds(86400)
+            .and()
+            .sessionManagement()
+                .sessionFixation().migrateSession()
+                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+            .maximumSessions(2);
+
+        // @formatter:on
     }
 
     @Bean
@@ -73,6 +74,11 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(11);
+    }
+
+    @Bean
+    public HttpSessionEventPublisher httpSessionEventPublisher() {
+        return new HttpSessionEventPublisher();
     }
 
 }
