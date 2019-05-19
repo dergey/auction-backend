23941dee8d6456df87ed6ab9c56c2274edd8b39a
dergey <dergey@gmail.com>
2019-05-19 22:05:41 +0300

    Added BadCredentialsException handle.

--- api/src/main/java/com/sergey/zhuravlev/auctionserver/controller/ErrorController.java
+++ api/src/main/java/com/sergey/zhuravlev/auctionserver/controller/ErrorController.java
@@ -6,6 +6,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -19,22 +20,29 @@ public class ErrorController {
 
     @ExceptionHandler(value = AccessDeniedException.class)
     public ResponseEntity<ExceptionDto> accessDeniedHandle(RuntimeException ex, WebRequest request) {
-        log.warn("Access denied", ex);
-        ExceptionDto error = new ExceptionDto(new Date(),  "Forbidden", request.getDescription(false));
+        log.debug("Access denied", ex);
+        ExceptionDto error = new ExceptionDto(new Date(),  "AccessDenied", request.getDescription(false));
         return new ResponseEntity<>(error, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(value = BadCredentialsException.class)
+    public ResponseEntity<ExceptionDto> badCredentialsHandle(RuntimeException ex, WebRequest request) {
+        log.debug("Bad credentials");
+        ExceptionDto error = new ExceptionDto(new Date(),  "BadCredentials", request.getDescription(false));
+        return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(value = {EntityNotFoundException.class, NotFoundException.class})
     public ResponseEntity<ExceptionDto> notFoundHandle(RuntimeException ex, WebRequest request) {
-        log.warn("Not found", ex);
-        ExceptionDto error = new ExceptionDto(new Date(), ex.getMessage(), request.getDescription(false));
+        log.debug("Not found", ex);
+        ExceptionDto error = new ExceptionDto(new Date(), "EntityNotFound", request.getDescription(false));
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(value = RuntimeException.class)
     public ResponseEntity<ExceptionDto> unexpectedSystemErrorHandle(RuntimeException ex, WebRequest request) {
-        log.warn("Unexpected system error", ex);
-        ExceptionDto error = new ExceptionDto(new Date(), ex.getMessage(), request.getDescription(false));
+        log.debug("Unexpected system error", ex);
+        ExceptionDto error = new ExceptionDto(new Date(), "UnexpectedSystemError", request.getDescription(false));
         return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
